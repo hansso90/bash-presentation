d997f8629206f14eba1268cdbb404bfce41c935f
Hans Lemkes <hanslemke@nn.nl>
2019-06-19 10:32:40 +0200

    fix echo -e issues

--- handson/02-variables/start.sh
+++ handson/02-variables/start.sh
@@ -3,6 +3,7 @@
 #variables
 set -o errexit
 set -o pipefail
+_max_sheets=6
 
 #set current dir
 MY_PATH=$(dirname "$0")
@@ -10,64 +11,94 @@ MY_PATH=$( cd "$MY_PATH" && pwd )
 
 #actually code
 ./sheet.sh "Lesson 02: variables"
-echo -e "For now let's focus on two types of variables:"
-echo -e "\033[0;31m\t Environment variables"
-echo -e "\t Shell variables\033[0m"
-echo -e "\n But how the can set is really different: "
-echo -e "\033[0;32m\t set"
-echo -e "\t env"
-echo -e "\t declare"
-echo -e "\t export\033[0m"
-read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 1 \ 5 ]' -n1
+echo -e "
+For now let's focus on two types of variables:
+    \033[0;31mEnvironment variables
+    Shell variables\033[0m
+
+But how to use the most of the commands. 
+    \033[0;32menv
+    declare
+    export\033[0m"
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 1 \ '$_max_sheets' ]' -n1
 
 ./sheet.sh "Lesson 02: variables"
-echo -e "\033[0;32mSet\033[0m"
-echo -e "\t Doesn't set shell nor environment variables (Misleading windows setting env.)"
-echo -e "\033[0;32mEnv\033[0m"
-echo -e "\t Can set environment variables for a single command"
-echo -e "\033[0;32mDeclare\033[0m"
-echo -e "\t Sets shell variables"
-echo -e "\033[0;32mExport\033[0m"
-echo -e "\t Makes shell variables environment variables"
-read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 2 \ 5 ]' -n1
+echo -e "
+    \033[0;32mSet\033[0m
+        Doesn't set shell nor environment variables (Misleading windows setting env.)
+    \033[0;32mEnv\033[0m
+        Can set environment variables for a single command
+    \033[0;32mDeclare\033[0m
+        Sets shell variables
+    \033[0;32mExport\033[0m
+        Makes shell variables environment variables
+Let's test this in bash.."
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 2 \ '$_max_sheets' ]' -n1
 
 ./sheet.sh "Lesson 02: variables"
-echo -e "Let's test this in bash...\n"
+echo -e "Let's test this in bash...
 
-echo -e "\033[0;32mset:\033[0m"
-echo -e "set x=5"
-set x=5
-echo -e "echo \$x: $x \n\n"
+\033[0;32menv:\033[0m
+Step 1: Setup
+    create ./example.sh << EOF 
+        \033[0;32m#!/usr/bin/env bash\033[0m
+        \033[0;33mecho\033[0m \"From example.sh: '\$y'\" 
+    EOF"
+echo -e "#!/usr/bin/env bash \n\n echo \"From example.sh: '\$y'\"" > example.sh
+echo -e "
+Step 2: Execute:
+    Command: 
+        env y=5 ./example.sh
 
-echo -e "\033[0;32menv:\033[0m"
-echo -e "create ./example.sh < \n\t\033[0;32m#!/usr/bin/env bash\033[0m\n\t\033[0;33mecho\033[0m \"From example.sh: '\$y'\" \n" && echo -e "#!/usr/bin/env bash \n\n echo \"From example.sh: '\$y'\"" > example.sh
-echo -e "env y=5 ./example.sh"
-env y=5 ./example.sh
-echo -e "Parent script \$y: $y"
+Step 3: Result
+    $(env y=5 ./example.sh)
+    Parent script \$y: $y"
 rm example.sh
-read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 3 \ 5 ]' -n1
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 3 \ '$_max_sheets' ]' -n1
 
 ./sheet.sh "Lesson 02: variables"
-echo -e "\033[0;32mdeclare:\033[0m"
-echo -e "create ./example.sh < \n\t\033[0;32m#!/usr/bin/env bash\033[0m\n\t\033[0;33mecho\033[0m \"From example.sh: '\$z'\" \n" && echo -e "#!/usr/bin/env bash \n\n echo \"From example.sh: '\$z'\"" > example.sh
-echo -e "declare z=6"
-declare z=6 
-./example.sh
-echo -e "Parent script \$z: $z \n\n"
+echo -e "\033[0;32mdeclare:\033[0m
+Step 1: Setup
+    create ./example.sh << EOF
+        \033[0;32m#!/usr/bin/env bash\033[0m
+        \033[0;33mecho\033[0m \"From example.sh: '\$z'\"
+    EOF"
+echo -e "#!/usr/bin/env bash \n\n echo \"From example.sh: '\$z'\"" > example.sh
+echo -e "
+Step 2: execute
+    declare z=6"
+    declare z=6
+
+echo -e "
+Step 3: Results
+    $(./example.sh)
+    Parent script \$z: $z \n\n"
 rm example.sh
 
-echo -e "\033[0;32mexport:\033[0m"
-echo -e "create ./example.sh < \n\t\033[0;32m#!/usr/bin/env bash\033[0m\n\t\033[0;33mecho\033[0m \"From example.sh: '\$q'\" \n" && echo -e "#!/usr/bin/env bash \n\n echo \"From example.sh: '\$q'\"" > example.sh
-echo -e "export z=6"
-export q=6 
-./example.sh
-echo -e "Parent script \$q: $q"
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 4 \ '$_max_sheets' ]' -n1
+./sheet.sh "Lesson 02: variables"
+
+echo -e "\033[0;32mexport:\033[0m
+Step 1: Setup
+    create ./example.sh << EOF 
+        \033[0;32m#!/usr/bin/env bash\033[0m
+        \033[0;33mecho\033[0m \"From example.sh: '\$q'\"
+    EOF"
+echo -e "#!/usr/bin/env bash \n\n echo \"From example.sh: '\$q'\"" > example.sh
+echo -e "
+Step 2: Execute 
+    export z=6"
+    export q=6 
+echo -e "
+Step 3: Results
+    $(./example.sh)
+    Parent script \$q: $q"
 rm example.sh
 
-read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 4 \ 5 ]' -n1
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 5 \ '$_max_sheets' ]' -n1
 ./sheet.sh "Lesson 02: variables"
 echo -e "
 Common Mistakes:
     - Spaces arround = sign to assign a value, correct: var=x"
 
-read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 5 \ 5 ]' -n1
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 6 \ '$_max_sheets' ]' -n1
--- handson/04-arrays/start.sh
+++ handson/04-arrays/start.sh
@@ -6,7 +6,7 @@ echo -e "\t\033[0;32m declare -a arrayName\033[0m"
 echo -e "\nAssociative arrays"
 echo -e "\t\033[0;32m declare -A arrayName\033[0m"
 
-read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 1 \ 1 ]' -n1 
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 1 \ 2 ]' -n1 
 
 ./sheet.sh "Lesson 04: arrays:"
 echo -e "Use of the array, let's create one..."
@@ -19,4 +19,4 @@ echo "Array \${array[@]}: ${array[*]}"
 echo "Array \${#array[@]}: ${#array[@]}"
 echo "Array \${array[1]}: ${array[1]}"
 
-read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 1 \ 1 ]' -n1 
+read -rsp $'\nPress any key to continue with the lesson.... \t\t\t\t Sheet: [ 2 \ 2 ]' -n1 
